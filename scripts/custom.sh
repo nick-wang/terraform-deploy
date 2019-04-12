d975062e71b0648755b29ddf9e194776f7bef8b3
Nick Wang <nwang@suse.com>
2019-04-12 10:51:03 +0800

    modify custom.sh to add more repos.

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -13,12 +13,22 @@ git checkout dev
 git pull local dev
 popd
 
-# Copy the files
+# Copy the files of formula
+# run as: salt '*' state.apply drbd
+#      or salt '*' state.apply drbd.global_confs
 cp -rf /root/Source/drbd-formula.git/drbd/         /srv/salt
 cp -rf /root/Source/timezone-formula.git/timezone/ /srv/salt
 
 cp /root/Source/drbd-formula.git/pillar.example     /srv/pillar/drbd/formula.sls
 cp /root/Source/timezone-formula.git/pillar.example /srv/pillar/timezone/formula.sls
+
+# Copy the files of modules/state
+# After any modification on master, should run:  salt '*' saltutil.sync_all
+mkdir -p /srv/salt/_modules
+mkdir -p /srv/salt/_states
+
+cp /root/Source/salt-drbd.git/salt/modules/drbd.py  /srv/salt/_modules
+cp /root/Source/salt-drbd.git/salt/states/drbd.py  /srv/salt/_states
 
 
 # Partitioning the /dev/vdb
@@ -56,3 +66,17 @@ fi
 
 # Need to reboot before try accept salt cluster
 reboot
+
+
+# Commands to run
+# Update modules/states/etc...:  	salt '*' saltutil.sync_all
+# highstate: 						salt '*' state.highstate
+#	refer to:	/srv/salt/top.sls
+# Formula:                          salt '*' state.apply drbd
+#	refer to:	/srv/salt/drbd/init.sls
+# Modules: 							salt '*' drbd.createmd
+#	refer to:	/srv/salt/_modules/drbd.py (func createmd)
+# States:							salt '*' state.apply drbd.xxx
+#									salt '*' state.apply drbd.dummy test=True
+#	refer to:	/srv/salt/drbd/xxx.sls (from formula but not init.sls)
+#   	func 	/srv/salt/_states/drbd.py (func dummy in xxx.sls)
